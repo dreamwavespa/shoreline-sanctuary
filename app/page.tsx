7457f792adc32146dcb47a107ab00cad99e8aeaa
dreamwavespa <darceylynn@gmail.com>
2026-09-15 19:25:37 -0400

    Mount Cadence ducky encounter

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { GameProvider } from "@/lib/store";
 import GameShell from "@/components/GameShell";
+import CadenceDuckyEncounter from "@/components/CadenceDuckyEncounter";
 
 export default function Home() {
   return (
     <GameProvider>
       <GameShell />
+      <CadenceDuckyEncounter />
     </GameProvider>
   );
 }
